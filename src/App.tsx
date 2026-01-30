cac98c1bcfa29ad2227ee722b4725b1a8306fd2a
Jessica <jessiberteloni@gmail.com>
2026-01-30 15:39:59 -0300

    Componente Categoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ListaCategorias from './components/categoria/listacategorias/ListaCategorias';
+import FormCategoria from './components/categoria/formcategoria/FormCategoria';
+import DeletarCategoria from './components/categoria/deletarcategoria/DeletarCategoria';
+import { AuthProvider } from './contexts/AuthContext';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
- 
+    return(
+       <>
+       <AuthProvider>
+				<ToastContainer/>
+				<BrowserRouter>
+					{/* <Navbar />  */}
+					<div className="min-h-[80vh]">
+						<Routes>
+							
+							{/* <Route path="/cadastro"	element={<Cadastro />}/> */}
+              				<Route path="/" element={<ListaCategorias />} />
+							<Route path="/categorias" element={<ListaCategorias/>} />
+							<Route path="/cadastrarcategoria" element={<FormCategoria />} />
+							<Route path="/editarcategoria/:id" element={<FormCategoria />} />
+							<Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
 
-
-return(
-  <>
-  </>
-)
-}
+						</Routes>
+					</div>
+					{/* <Footer /> */}
+				</BrowserRouter>
+			</AuthProvider>
+       </>
+     )
+    }
 
 export default App
