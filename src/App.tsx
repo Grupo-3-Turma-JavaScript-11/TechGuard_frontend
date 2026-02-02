4ed551ab707ab57e2abae92904a7167a8738a7a2
Jessica <jessiberteloni@gmail.com>
2026-02-02 09:57:44 -0300

    Ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,32 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-<<<<<<< HEAD
-import ListaSeguros from './components/seguro/listaseguros/ListaSeguros'
-import DeletarSeguro from './components/seguro/deletarseguro/DeletarSeguro'
-import FormSeguro from './components/seguro/formseguro/FormSeguro'
 import { AuthProvider } from './contexts/AuthContext'
+import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify'
-import ListaCategorias from './components/categoria/listacategorias/ListaCategorias'
-import FormCategoria from './components/categoria/formcategoria/FormCategoria';
-import DeletarCategoria from './components/categoria/deletarcategoria/DeletarCategoria';
-
-
-function App() {
-  return(
-    <>
-     <AuthProvider>
-			  <ToastContainer/>
-			  <BrowserRouter>
-				  {/* <Navbar />  */}
-				    <div className="min-h-[80vh]">
-					  <Routes>
-						
-						  {/* <Route path="/cadastro"	element={<Cadastro />}/> */}
-          	  <Route path="/" element={<ListaCategorias />} />
-						  <Route path="/categorias" element={<ListaCategorias/>} />
-						  <Route path="/cadastrarcategoria" element={<FormCategoria />} />
-						  <Route path="/editarcategoria/:id" element={<FormCategoria />} />
-						  <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
-              <Route path='/seguros' element={<ListaSeguros />} />
-              <Route path='/cadastrarseguro' element={<FormSeguro />} />
-              <Route path='/editarseguro/:id' element={<FormSeguro />} />
-              <Route path='/deletarseguro/:id' element={<DeletarSeguro />} />
-            </Routes>
-            </div>
-        </BrowserRouter>
-      </AuthProvider>
-    </>
-  )
-=======
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Home from './pages/home/Home';
+import Home from './pages/home/Home'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
 
 function App() {
 
 return(
   <>
-  <Navbar />
-  <Home />
-  <Footer />
+  <AuthProvider>
+				<ToastContainer />
+				<BrowserRouter>
+					<Navbar />
+
+						<Routes>
+							<Route path="/" element={<Home />} />
+							{/* <Route path="/login" element={<Login />} />
+							<Route path="/cadastro"	element={<Cadastro />}/> */}
+						</Routes>
+			
+					<Footer />
+				</BrowserRouter>
+			</AuthProvider>
   </>
 );
->>>>>>> 418bb9dc1760b58e781f40c91da16a36fca99ae0
 }
 
 export default App
